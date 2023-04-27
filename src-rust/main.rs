b7c3861ee07b33c12e4050de7be6444a6b1082db
Eric Wang <eric.wang401@gmail.com>
2023-04-27 01:33:13 +0000

    add  custom headers to websocket connection

--- src-rust/main.rs
+++ src-rust/main.rs
@@ -1,20 +1,23 @@
-use axum::extract::ws::{Message as AMessage, CloseFrame as ACloseFrame};
+use axum::extract::ws::{CloseFrame as ACloseFrame, Message as AMessage};
 use axum::{
     body::{boxed, Body, BoxBody},
     extract::ws::{WebSocket, WebSocketUpgrade},
-    http::{Request, Response, StatusCode, Uri},
+    http::{Response, StatusCode, Uri},
     response::IntoResponse,
     routing::get,
     Router,
 };
 use futures_util::{sink::SinkExt, stream::StreamExt};
+use httparse::{Header, Request, EMPTY_HEADER};
 use std::{net::SocketAddr, path::PathBuf};
 use tokio::join;
-use tokio_tungstenite::{connect_async, tungstenite::{
-    Message as TMessage,
-    protocol::CloseFrame as TCloseFrame,
-    protocol::frame::coding::CloseCode as TCloseCode,
-}};
+use tokio_tungstenite::{
+    connect_async,
+    tungstenite::{
+        protocol::frame::coding::CloseCode as TCloseCode, protocol::CloseFrame as TCloseFrame,
+        Message as TMessage,
+    },
+};
 use tower_http::cors::{Any, CorsLayer};
 use tower_http::services::ServeDir;
 
@@ -52,7 +55,11 @@ async fn demo(ws: WebSocketUpgrade) -> impl IntoResponse {
 
             println!("demo ws msg received: {:?}", msg);
 
-            if socket.send("please don't contact me again. I'm on the no-call list".into()).await.is_err() {
+            if socket
+                .send("please don't contact me again. I'm on the no-call list".into())
+                .await
+                .is_err()
+            {
                 // client disconnected
                 return;
             }
@@ -67,7 +74,35 @@ async fn ws_upgrader_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
 async fn handle_socket(client_socket: WebSocket) {
     let (mut client_sender, mut client_receiver) = client_socket.split();
 
-    let (remote_socket, _) = connect_async("ws://localhost:3000/demo")
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut remote_request = Request::new(&mut headers);
+    remote_request.method = Some("GET");
+    remote_request.version = Some(1);
+    remote_request.path = Some("ws://localhost:3000/demo");
+    let mut actual_headers = [Header {
+        name: "sec-websocket-key",
+        value: b"xI6cUQeu/u73MZN6C0ChtQ==",
+    },
+    Header {
+        name: "host",
+        value: b"localhost:3000",
+    },
+    Header {
+        name: "sec-websocket-version",
+        value: b"13",
+    },
+    Header {
+        name: "connection",
+        value: b"Upgrade",
+    },
+    Header {
+        name: "upgrade",
+        value: b"websocket",
+    },
+    ];
+    remote_request.headers = &mut actual_headers;
+
+    let (remote_socket, _) = connect_async(remote_request)
         .await
         .expect("Failed to connect to remote websocket");
     let (mut remote_sender, mut remote_receiver) = remote_socket.split();
@@ -123,10 +158,15 @@ fn convert_tungstenite_to_axum(tungstenite_msg: TMessage) -> AMessage {
         TMessage::Ping(payload) => AMessage::Ping(payload.into()),
         TMessage::Pong(payload) => AMessage::Pong(payload.into()),
         TMessage::Close(payload) => {
-            let (code, reason) = payload.map(|c| (c.code, c.reason)).unwrap_or((TCloseCode::from(1000), String::new().into()));
-            let close_frame = ACloseFrame { code: code.into(), reason };
+            let (code, reason) = payload
+                .map(|c| (c.code, c.reason))
+                .unwrap_or((TCloseCode::from(1000), String::new().into()));
+            let close_frame = ACloseFrame {
+                code: code.into(),
+                reason,
+            };
             AMessage::Close(Some(close_frame))
-        },
+        }
         TMessage::Frame(_) => panic!("Frame messages are not supported"),
     }
 }
