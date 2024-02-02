306832df1dc2e2cdfa00f44dcb4b10c5adfa3b16
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2024-02-01 22:22:57 -0600

    Add more logging points 😩😩

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ This file is a running track of new features and fixes to each version of the pa
 
 This project follows [Semantic Versioning](http://semver.org) guidelines.
 
+## v2.3.0
+
+### Changes
+
+- Added another debug log point before constructing the websocket request.
+
 ## v2.2.0
 
 ### Changes
--- src-rust/src/util/api/proxmox.rs
+++ src-rust/src/util/api/proxmox.rs
@@ -3,6 +3,7 @@ use native_tls::TlsConnector;
 use tokio_tungstenite::Connector;
 use tokio_tungstenite::tungstenite::handshake::client::{generate_key, Request};
 use tokio_tungstenite::tungstenite::http::header::{CONNECTION, SEC_WEBSOCKET_KEY, SEC_WEBSOCKET_PROTOCOL, SEC_WEBSOCKET_VERSION, UPGRADE};
+use tracing::{debug, debug_span};
 use urlencoding::encode;
 
 use crate::util::api::novnc::NoVncCredentials;
@@ -14,59 +15,67 @@ pub enum Credentials {
 }
 
 pub fn build_ws_request(credentials: Credentials) -> (Request, Connector) {
-    let mut request = Request::builder()
-        .header(SEC_WEBSOCKET_KEY, generate_key())
-        .header(SEC_WEBSOCKET_VERSION, "13")
-        .header(SEC_WEBSOCKET_PROTOCOL, "binary")
-        .header(CONNECTION, "Upgrade")
-        .header(UPGRADE, "websocket");
+    let span = debug_span!("Constructing WS request");
 
-    let do_not_verify_tls = var("DANGEROUS_DISABLE_TLS_VERIFICATION")
-        .unwrap_or("false".to_string())
-        .to_lowercase() == "true";
-    let tls_connector = TlsConnector::builder()
-        .danger_accept_invalid_hostnames(do_not_verify_tls)
-        .danger_accept_invalid_certs(do_not_verify_tls)
-        .build()
-        .unwrap();
-    let tls_connector = Connector::NativeTls(tls_connector);
+    span.in_scope(|| {
+        let mut request = Request::builder()
+            .header(SEC_WEBSOCKET_KEY, generate_key())
+            .header(SEC_WEBSOCKET_VERSION, "13")
+            .header(SEC_WEBSOCKET_PROTOCOL, "binary")
+            .header(CONNECTION, "Upgrade")
+            .header(UPGRADE, "websocket");
+        debug!("Request headers constructed");
 
-    match credentials {
-        Credentials::NoVnc(credentials) => {
-            let path = format!(
-                "wss://{node_fqdn}:{node_port}/api2/json/nodes/{node}/qemu/{vmid}/vncwebsocket?port={vnc_port}&vncticket={ticket}",
-                node_fqdn = credentials.node_fqdn,
-                node_port = credentials.node_port,
-                node = credentials.node_pve_name,
-                vmid = credentials.vmid,
-                vnc_port = credentials.port,
-                ticket = encode(&credentials.ticket)
-            );
+        let do_not_verify_tls = var("DANGEROUS_DISABLE_TLS_VERIFICATION")
+            .unwrap_or("false".to_string()).parse::<bool>().unwrap_or(false);
+        debug!("TLS verification disabled: {do_not_verify_tls}", do_not_verify_tls = do_not_verify_tls);
+        let tls_connector = TlsConnector::builder()
+            .danger_accept_invalid_hostnames(do_not_verify_tls)
+            .danger_accept_invalid_certs(do_not_verify_tls)
+            .build()
+            .unwrap();
+        debug!("TLS connector constructed");
+        let tls_connector = Connector::NativeTls(tls_connector);
 
-            request = request
-                .header("Cookie", format!("PVEAuthCookie={}", credentials.pve_auth_cookie))
-                .header("Host", format!("{}:{}", credentials.node_fqdn, credentials.node_port))
-                .uri(path);
+        match credentials {
+            Credentials::NoVnc(credentials) => {
+                let path = format!(
+                    "wss://{node_fqdn}:{node_port}/api2/json/nodes/{node}/qemu/{vmid}/vncwebsocket?port={vnc_port}&vncticket={ticket}",
+                    node_fqdn = credentials.node_fqdn,
+                    node_port = credentials.node_port,
+                    node = credentials.node_pve_name,
+                    vmid = credentials.vmid,
+                    vnc_port = credentials.port,
+                    ticket = encode(&credentials.ticket)
+                );
+
+                request = request
+                    .header("Cookie", format!("PVEAuthCookie={}", credentials.pve_auth_cookie))
+                    .header("Host", format!("{}:{}", credentials.node_fqdn, credentials.node_port))
+                    .uri(path);
+            }
+            Credentials::XTerm(credentials) => {
+                let path = format!(
+                    "wss://{node_fqdn}:{node_port}/api2/json/nodes/{node}/qemu/{vmid}/vncwebsocket?port={vnc_port}&vncticket={ticket}",
+                    node_fqdn = credentials.node_fqdn,
+                    node_port = credentials.node_port,
+                    node = credentials.node_pve_name,
+                    vmid = credentials.vmid,
+                    vnc_port = credentials.port,
+                    ticket = encode(&credentials.ticket)
+                );
+
+                request = request
+                    .header("Cookie", format!("PVEAuthCookie={}", credentials.pve_auth_cookie))
+                    .header("Host", format!("{}:{}", credentials.node_fqdn, credentials.node_port))
+                    .uri(path);
+            }
         }
-        Credentials::XTerm(credentials) => {
-            let path = format!(
-                "wss://{node_fqdn}:{node_port}/api2/json/nodes/{node}/qemu/{vmid}/vncwebsocket?port={vnc_port}&vncticket={ticket}",
-                node_fqdn = credentials.node_fqdn,
-                node_port = credentials.node_port,
-                node = credentials.node_pve_name,
-                vmid = credentials.vmid,
-                vnc_port = credentials.port,
-                ticket = encode(&credentials.ticket)
-            );
 
-            request = request
-                .header("Cookie", format!("PVEAuthCookie={}", credentials.pve_auth_cookie))
-                .header("Host", format!("{}:{}", credentials.node_fqdn, credentials.node_port))
-                .uri(path);
-        }
-    }
+        let request = request.body(()).unwrap();
 
-    let request = request.body(()).unwrap();
+        debug!("WS request constructed");
 
-    (request, tls_connector)
+        (request, tls_connector)
+    })
 }
